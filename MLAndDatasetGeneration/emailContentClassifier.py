cde8c186a40e4ee33b3695f4edd472d8146bcf22
Karanpreet Raja <86526687+KaranpreetRaja@users.noreply.github.com>
2023-05-06 02:07:52 -0400

    Finishes Tokenizer Classifier

--- MLAndDatasetGeneration/emailContentClassifier.py
+++ MLAndDatasetGeneration/emailContentClassifier.py
@@ -5,132 +5,79 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 import pandas as pd
 from sklearn.metrics import f1_score, precision_score, recall_score, accuracy_score
-from sklearn.model_selection import KFold
-from transformers import BertTokenizer
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
+from transformers import AutoTokenizer
+from transformers import DistilBertTokenizer, DistilBertModel
 
-# Defines custom dataset for PyTorch
-class CustomDataset(Dataset):
-    def __init__(self, data, tokenizer, max_sequence_length):
+
+
+class EmailDataset(Dataset):
+    def __init__(self, data, tokenizer, max_len):
         self.data = data
         self.tokenizer = tokenizer
-        self.max_sequence_length = max_sequence_length
+        self.max_len = max_len
 
     def __len__(self):
         return len(self.data)
 
-    def __getitem__(self, idx):
-        row = self.data.iloc[idx]
-        text_features = self.tokenizer(row['title'] + row['body'], padding='max_length', truncation=True, return_tensors='pt')
-        actual_sequence_length = text_features['input_ids'].size(1)
-        padding_length = self.max_sequence_length - actual_sequence_length
-        if padding_length < 0:
-            padding_length = 0
-        elif padding_length >= self.max_sequence_length:
-            padding_length = 0
-        text_features_padded = {key: torch.nn.functional.pad(value[0], (0, 0, 0, padding_length)) if padding_length > 0 else value[0] for key, value in text_features.items()}
-        numerical_features = torch.tensor([row['tld'], row['has_return_path'], row['x_bulkmail']], dtype=torch.float32)
-        label = torch.tensor(row['label'], dtype=torch.float32)
-        return text_features_padded, numerical_features, label
+    def __getitem__(self, index):
 
 
-# Defines binary classifier using PyTorch
+        row = self.data.iloc[index]
+        title = row["title"]
+        body = row["body"]
+        tld = row["tld"]
+        has_return_path = row["has_return_path"]
+        x_bulkmail = row["x_bulkmail"]
+        label = row["label"]
+
+        # Tokenize and create attention mask for title and body
+        title_encodings = self.tokenizer(title, truncation=True, max_length=self.max_len, padding="max_length", return_tensors="pt")
+        body_encodings = self.tokenizer(body, truncation=True, max_length=self.max_len, padding="max_length", return_tensors="pt")
+
+        # print(f"title: {title}")
+        # print(f"title_encodings['input_ids'].shape: {title_encodings['input_ids'].shape}")
+        # print(f"title_encodings['attention_mask'].shape: {title_encodings['attention_mask'].shape}")
+        # print(f"body: {body}")
+        # print(f"body_encodings['input_ids'].shape: {body_encodings['input_ids'].shape}")
+
+        return {
+            "title_input_ids": title_encodings["input_ids"].squeeze(0),
+            "title_attention_mask": title_encodings["attention_mask"].squeeze(0),
+            "body_input_ids": body_encodings["input_ids"].squeeze(0),
+            "body_attention_mask": body_encodings["attention_mask"].squeeze(0),
+            "tld": torch.tensor(tld, dtype=torch.long),
+            "has_return_path": torch.tensor(has_return_path, dtype=torch.float32),
+            "x_bulkmail": torch.tensor(x_bulkmail, dtype=torch.float32),
+            "targets": torch.tensor(label, dtype=torch.float32),
+        }
+
+
+
 class BinaryClassifier(nn.Module):
-    def __init__(self, input_size):
+    def __init__(self, distilbert_model):
         super(BinaryClassifier, self).__init__()
-        self.fc1 = nn.Linear(input_size, 64)
-        self.fc2 = nn.Linear(64, 32)
-        self.fc3 = nn.Linear(32, 1)
-        self.sigmoid = nn.Sigmoid()
+        self.distilbert = distilbert_model
+        self.dropout = nn.Dropout(0.3)
+        self.linear = nn.Linear(self.distilbert.config.hidden_size, 1)
 
-    def forward(self, x):
-        x = self.fc1(x)
-        x = nn.functional.relu(x)
-        x = self.fc2(x)
-        x = nn.functional.relu(x)
-        x = self.fc3(x)
-        x = self.sigmoid(x)
-        return x
-
-# Defines Deep Q-Network (DQN) agent w/ PyTorch
-class DQNAgent():
-    def __init__(self, state_size, action_size):
-        self.state_size = state_size
-        self.action_size = action_size
-        self.memory = []
-        self.gamma = 0.95
-        self.epsilon = 1.0
-        self.epsilon_decay = 0.995
-        self.epsilon_min = 0.01
-        self.batch_size = 32
-        self.model = self._build_model()
-
-    def _build_model(self):
-        model = nn.Sequential(
-            nn.Linear(self.state_size, 64),
-            nn.ReLU(),
-            nn.Linear(64, 32),
-            nn.ReLU(),
-            nn.Linear(32, self.action_size)
-        )
-        return model
-
-    def remember(self, state, action, reward, next_state, done):
-        self.memory.append((state, action, reward, next_state, done))
-
-    def act(self, state):
-        if np.random.rand() <= self.epsilon:
-            return np.random.choice(self.action_size)
-        state = torch.FloatTensor(state)
-        q_values = self.model(state)
-        return torch.argmax(q_values).item()
-
-    def replay(self):
-        if len(self.memory) < self.batch_size:
-            return
-        batch = np.random.choice(len(self.memory), self.batch_size, replace=False)
-        states, targets = [], []
-        for idx in batch:
-            state, action, reward, next_state, done = self.memory[idx]
-            target = reward
-            if not done:
-                next_state = torch.FloatTensor(next_state)
-                next_q_values = self.model(next_state)
-                target = reward + self.gamma * torch.max(next_q_values)
-            state = torch.FloatTensor(state)
-            q_values = self.model(state)
-            q_values[action] = target
-            states.append(state)
-            targets.append(q_values)
-        states = torch.stack(states)
-        targets = torch.stack(targets)
-        self.model.train()
-        optimizer = optim.Adam(self.model.parameters(), lr=0.001)
-        criterion = nn.MSELoss()
-        for epoch in range(1):
-            optimizer.zero_grad()
-            outputs = self.model(states)
-            loss = criterion(outputs, targets)
-            loss.backward()
-            optimizer.step()
-
-    def decay_epsilon(self):
-        if self.epsilon > self.epsilon_min:
-            self.epsilon *= self.epsilon_decay
-
-    def load(self, name):
-        self.model.load_state_dict(torch.load(name))
-
-    def save(self, name):
-        torch.save(self.model.state_dict(), name)
+    def forward(self, input_ids, attention_mask):
+        outputs = self.distilbert(input_ids, attention_mask=attention_mask)
+        pooled_output = outputs[0][:, 0]
+        pooled_output = self.dropout(pooled_output)
+        logits = self.linear(pooled_output)
+        return logits.squeeze(-1)
 
 
-# tokenizes the data and Truncate
+# tokenizes the data and Truncate and add padding to the data
 def tokenize_and_truncate(string, tokenizer, max_len):
     tokens = tokenizer.tokenize(string)
     tokens = tokens[:max_len-2]
-    return tokens
+    # Add padding tokens
+    tokens += ['[PAD]'] * (max_len - 2 - len(tokens))
+    return " ".join(tokens)
+
 
 
 def load_data(tokenizer):
@@ -139,7 +86,7 @@ def load_data(tokenizer):
 
 
     # get tokenizer max length
-    max_len = tokenizer.max_model_input_sizes['bert-base-uncased']
+    max_len = tokenizer.max_model_input_sizes['distilbert-base-uncased']
 
 
 
@@ -171,8 +118,10 @@ def load_data(tokenizer):
     # 6. X-keywords (give me a comma separated list of all x-keywords, if doesn't exist, then empty)
     data["x_keywords"] = data["x_keywords"].fillna("").str.split(",")
 
-    # 7. label (Yes if spam, No if not-spam)
-    data["label"] = data["label"].map({"Yes": 1, "No": 0}).astype(np.float32)
+    print(data["label"])
+
+    # 7. label (Yes if spam, No if not-spam) (convert to 1 and 0)
+    data["label"] = data["label"].replace({"Yes": 1, "No": 0})
 
     # Define the features and target
     features = ["title", "body", "tld", "has_return_path", "x_bulkmail"]
@@ -182,10 +131,13 @@ def load_data(tokenizer):
 
     target = "label"
 
-    # Create X and y
-    dataset = CustomDataset(data, tokenizer, max_len)
+    X = data[features].values
+    y = data[target].values
 
-    return dataset
+    print(data["label"])
+
+    return data
+
 
 # Chooses the device (CPU or GPU)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -194,69 +146,75 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 if torch.cuda.is_available():
     print("Using CUDA!")
 
+
+
 # Loads the data
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
-dataset = load_data(tokenizer)
-
-# Splits the data into training and testing sets
-train_size = int(0.8 * len(dataset))
-test_size = len(dataset) - train_size
-train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
-
-# Creates the data loaders
-train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased', do_lower_case=True)
+max_len = tokenizer.max_model_input_sizes['distilbert-base-uncased']
+data = load_data(tokenizer)
+train_data, test_data = train_test_split(data, test_size=0.2, random_state=42)
+train_dataset = EmailDataset(train_data, tokenizer, max_len)
+test_dataset = EmailDataset(test_data, tokenizer, max_len)
 
 
+# Instantiate the BERT model 
+distilbert_model = DistilBertModel.from_pretrained('distilbert-base-uncased')
 
+# Instantiate the classifier
+classifier = BinaryClassifier(distilbert_model).to(device)
 
-# Creates the binary classifier and trains it using PyTorch lib
-clf = BinaryClassifier(input_size=3)
-criterion = nn.BCELoss()
-optimizer = optim.Adam(clf.parameters(), lr=0.001)
-num_epochs = 10
+# Define the loss function and optimizer
+criterion = nn.BCEWithLogitsLoss()
+optimizer = optim.Adam(filter(lambda p: p.requires_grad, classifier.parameters()), lr=2e-5)
+
+# Define the data loader and train the model
+train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=4, shuffle=False)
+num_epochs = 5
+
+count = 0
+torch.cuda.empty_cache()
+# Training loop go BRRRRRRRRR
+accumulation_steps = 4
+optimizer.zero_grad()
 for epoch in range(num_epochs):
+    print("Epoch: ", epoch)
     for batch in train_loader:
-        text_features, numerical_features, labels = batch
-        optimizer.zero_grad()
-        outputs = clf(numerical_features)
-        loss = criterion(outputs, labels)
+        count += 1
+        print("Batch: ", count)
+        title_input_ids = batch["title_input_ids"].to(device)
+        title_attention_mask = batch["title_attention_mask"].to(device)
+        body_input_ids = batch["body_input_ids"].to(device)
+        body_attention_mask = batch["body_attention_mask"].to(device)
+        targets = batch["targets"].to(device)
+        title_outputs = classifier(title_input_ids, title_attention_mask)
+        body_outputs = classifier(body_input_ids, body_attention_mask)
+        outputs = (title_outputs + body_outputs) / 2
+        loss = criterion(outputs, targets)
         loss.backward()
-        optimizer.step()
-    print("Epoch {}/{} - loss: {:.4f}".format(epoch+1, num_epochs, loss.item()))
+        if (count % accumulation_steps) == 0:
+            optimizer.step()
+            optimizer.zero_grad()
 
-# # Creates DQN agent and train it using PyTorch
-# state_size = X_train.shape[1]
-# action_size = 2
-# agent = DQNAgent(state_size, action_size)
-# num_episodes = 1000
-# for episode in range(num_episodes):
-#     state = X_train[0]
-#     done = False
-#     while not done:
-#         action = agent.act(state)
-#         next_state = X_train[action]
-#         reward = y_train[action]
-#         agent.remember(state, action, reward, next_state, done)
-#         state = next_state
-#         agent.replay()
-#     agent.decay_epsilon()
-#     if (episode+1) % 100 == 0:
-#         print("Episode {}/{}".format(episode+1, num_episodes))
 
-# Evaluates the binary classifier
-y_test = []
+# Evaluate the trained model on a test set
 y_pred = []
-clf.eval()
-with torch.no_grad():
-    for batch in test_loader:
-        text_features, numerical_features, labels = batch
-        outputs = clf(numerical_features)
-        y_test.extend(labels.numpy())
-        y_pred.extend(np.where(outputs.numpy() > 0.5, 1, 0))
+y_test = []
+for batch in test_loader:
+    title_input_ids = batch["title_input_ids"].to(device)
+    title_attention_mask = batch["title_attention_mask"].to(device)
+    body_input_ids = batch["body_input_ids"].to(device)
+    body_attention_mask = batch["body_attention_mask"].to(device)
+    targets = batch["targets"].to(device)
+    with torch.no_grad():
+        title_outputs = classifier(title_input_ids, title_attention_mask)
+        body_outputs = classifier(body_input_ids, body_attention_mask)
+        outputs = (title_outputs + body_outputs) / 2
+        outputs = torch.sigmoid(outputs)
+        y_pred.extend(torch.round(outputs).tolist())
+        y_test.extend(targets.tolist())
 
-y_test = np.array(y_test)
-y_pred = np.array(y_pred)
+
 
 #prints f1 score, precision, recall, accuracy
 print("F1 score: {:.2f}".format(f1_score(y_test, y_pred)))
@@ -266,5 +224,4 @@ print("Accuracy: {:.2f}".format(accuracy_score(y_test, y_pred)))
 
 
 # Saves trained models
-clf.save("binary_classifier.pt")
-# agent.save("dqn_agent.pt")
+torch.save(classifier.state_dict(), "binary_classifier.pt")
